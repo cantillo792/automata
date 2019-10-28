b9c06c80b7b08c390c29239dba1bfe9a8e75e42f
cantillo792@gmail.com <cantillo792@gmail.com>
2019-10-28 12:12:16 -0500

    automata

---
+++ evaluador/automata/transicion.py
@@ -0,0 +1,14 @@
+from grafo.arista import Arista
+
+class Transicion(Arista):
+    def __init__(self, valor, simbolo, tope, agregar,destino):
+        super().__init__(valor, destino)
+        self.simbolo = simbolo
+        self.tope = tope 
+        self.agregar = agregar
+        self.dict = {}
+        self.dict['transicion'] = self.__str__()
+        self.dict['destino'] = self.destino.getValor()
+
+    def __str__(self):
+        return self.simbolo+","+self.tope+"/"+str(self.agregar)
---
+++ evaluador/evaluador.py
@@ -0,0 +1,32 @@
+from automata.automata import *
+
+class Evaluador:
+    def __init__(self):
+        self.automata = Automata()
+        self.automata.addEstado("p", False)
+        self.automata.addEstado("q", False)
+        self.automata.addEstado("r", True)
+        self.automata.conectar("a","#","#a","p","p")
+        self.automata.conectar("b","#","#b","p","p")
+        self.automata.conectar("a","a","aa","p","p")
+        self.automata.conectar("b","a","ab","p","p")
+        self.automata.conectar("a","b","ba","p","p")
+        self.automata.conectar("b","b","bb","p","p")
+        self.automata.conectar("c","#","#","p","q")
+        self.automata.conectar("c","b","b","p","q")
+        self.automata.conectar("c","a","a","p","q")
+        self.automata.conectar("b","b","*","q","q")
+        self.automata.conectar("a","a","*","q","q")
+        self.automata.conectar("*","#","#","q","r")
+
+    def evaluar(self, palabra):
+        pasos = self.automata.Evaluar(palabra)
+        if pasos[-1]['estadoActual'] == "r":
+            aceptada = True
+        else:
+            aceptada = False
+        print(str(pasos)+str(aceptada))
+
+evaluador = Evaluador()
+evaluador.evaluar("aabcbaa")
+
